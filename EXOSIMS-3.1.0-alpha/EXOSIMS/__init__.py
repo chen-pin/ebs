db4c1e86ad9176c68cff34ca328717a22830d564
Pin Chen <Pin.Chen@jpl.nasa.gov>
2023-05-05 12:58:05 -0700

    Replace EXOSIMS 3.1-alpha with EXOSIMS 3.1.1 (latest stable ver)

--- EXOSIMS-3.1.0-alpha/EXOSIMS/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-name = "EXOSIMS"
-__version__ = "3.1.0-alpha-ChenP"
-
-# Set up a default logging handler to avoid "No handler found" warnings.
-# Other handlers can add to this one.
-logging.getLogger(__name__).addHandler(logging.NullHandler())
