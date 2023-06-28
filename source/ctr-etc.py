6bdfd44ba37e0c5a93849bc1f1702e0120fbbe9a
Pin Chen <Pin.Chen@jpl.nasa.gov>
2023-06-28 13:16:28 -0700

    Write function converting Excel file to JSON

--- source/ctr-etc.py
+++ source/ctr-etc.py
@@ -10,9 +10,11 @@ import pandas as pd
 def read_excel():
     sheet_name = ['KPP, no WFE', 'WFE', 'WFSC-PP factor']
     input_dict = pd.read_excel("../inputs/input.xlsx", sheet_name=sheet_name)
-    print(input_dict)
-    json_str = input_dict.to_json()
-    print(json_str)
+    for key in input_dict.keys():
+        print(key)
+        json_str = input_dict[key].to_json()
+        print(json_str)
+
 
 if __name__ == '__main__':
     read_excel()
