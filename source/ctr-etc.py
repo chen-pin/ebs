ff85f5898683ea010105f4315b4d9af6929e3fcd
Pin Chen <Pin.Chen@jpl.nasa.gov>
2023-07-02 18:22:18 -0700

    Draft first version of input JSON file

--- source/ctr-etc.py
+++ source/ctr-etc.py
@@ -5,15 +5,16 @@
 
 import os 
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+import json as js
+#import matplotlib.pyplot as plt
+#import pandas as pd
 import EXOSIMS.MissionSim as ems
 
 
 class ErrorBudget(ems.MissionSim):
     """Sub-class of `EXOSIMS.MissionSim.MissionSim` to compute exposure time"""
 
-    def __init__(self, input_filename='input.xlsx'):
+    def __init__(self, input_filename="sample.json"):
         self.input_dir = os.path.join("..", "inputs")
         self.input_path = os.path.join(self.input_dir, input_filename)
 
@@ -29,7 +30,12 @@ class ErrorBudget(ems.MissionSim):
 #            for key in input_dict.keys():
 #                json_str = input_dict[key].to_json(f)
 
+    def load_json(self):
+        with open(os.path.join(self.input_path)) as input_json:
+            input_dict = js.load(input_json)
+           # print(input_dict)
+            print(input_dict['modules'].keys())
 
 if __name__ == '__main__':
     x = ErrorBudget()
-    x.xlsx2json()
+    x.load_json()
