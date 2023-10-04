3b63e5d3dac9a822759cd01cec1cbb10f9537a81
Sarah Steiger <ssteiger@stsci.edu>
2023-10-04 09:09:08 -0400

    add documentation to utils functions

--- ebs/utils.py
+++ ebs/utils.py
@@ -4,19 +4,27 @@ import os
 import json as js
 
 
-def read_csv(filename):
-    data = np.loadtxt(open(filename, "rb"), delimiter=',', skiprows=0)
+def read_csv(filename, skiprows=0):
+    """
+    returns the data contained in a csv file as a numpy array.
+    Values are separated by commas and rows are separated by newlines
+    :param filename: str, fully qualified path to the csv file
+    :param skiprows: int, number of rows to skip at the top of the csv file
+    :return: numpy array, data contained in the csv file
+    """
+    data = np.loadtxt(open(filename, "rb"), delimiter=',', skiprows=skiprows)
     return data
 
 
 def generate_pp_json(json_file, wfe, wfsc, sensitivity):
     """
+    appends the wavefront error, sensitivity, and sensing and control coefficient to an existing json file
     overwrites the original json file
-    :param json_file:
-    :param wfe:
-    :param wfsc:
-    :param sensitivity:
-    :return:
+    :param json_file: str, fully qualified path to a json file
+    :param wfe: array or list, wavefront error data
+    :param wfsc: array or list, wavefront sensing and control data
+    :param sensitivity: array or list, sensitivity coefficient data
+    :return: None
     """
     with open(json_file) as f:
         input_dict = js.load(f)
