e7f61293e1977cd738bbbf6f817fe24598d66da4
Pin Chen <Pin.Chen@jpl.nasa.gov>
2023-11-12 17:45:22 -0800

    WIP: Add script to run ebs in mcmc mode

--- run_mcmc_ebs.py
+++ run_mcmc_ebs.py
@@ -2,7 +2,13 @@ import ebs.error_budget as eb
 
 print("UPDATE HDF FILENAME IN YAML CONFIG. FILE IF NECESSARY!")
 config_filename = input("Enter name of YAML configuration file:  ")
+mode = input("Choose single-process (s) or parallel run (p):  ")
 
 march = eb.ErrorBudgetMcmc(config_filename)
-march.run_mcmc()
+if mode == 's':
+    march.run_mcmc(parallel=False)
+elif mode == 'p':
+    march.run_mcmc(parallel=True)
+else:
+    march.run_mcmc(parallel=False)
 
