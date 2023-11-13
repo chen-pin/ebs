fb243054c26e6a68d286a63690147b8d4f200d0e
Pin Chen <Pin.Chen@jpl.nasa.gov>
2023-11-12 17:11:37 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="ebs",
-    version="0.0.2",
+    version="0.5",
     author="Pin Chen",
     author_email="pin.chen@jpl.nasa.gov",
     description="Software for running error budget calculations for HWO using EXOSIMS",
