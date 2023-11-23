de271b75fd262eb0baed776df756d733d3ae1a1a
Pin Chen <Pin.Chen@jpl.nasa.gov>
2023-11-22 16:23:28 -0800

    Add chi-square function

--- ebs/log_pdf.py
+++ ebs/log_pdf.py
@@ -11,3 +11,12 @@ def inverse_bounded(x, lower_bound, upper_bound):
     if lower_bound < x < upper_bound:
         return -np.log(x)
     return -np.inf
+
+
+def chi_square(x, center, e_scale, lower_bound=0.0, upper_bound=None):
+    if x < lower_bound:
+        return -np.inf
+    if upper_bound != None:
+        if x > upper_bound:
+            return -np.inf
+    return -0.5*((x-center)/e_scale)**2
