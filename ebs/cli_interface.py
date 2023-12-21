627c56c35c8b6be7c464f9cf9e92896785041832
Sarah Steiger <ssteiger@stsci.edu>
2023-12-21 14:50:37 -0500

    update call to plotting script in CLI interface

--- ebs/cli_interface.py
+++ ebs/cli_interface.py
@@ -68,5 +68,6 @@ def main():
 
     save_name = f'inttime_vs_{subparameter if subparameter else parameter}.pdf'
     plot_ebs_output(error_budget, spectral_dict, parameter if not subparameter else subparameter, values,
-                    result_dict['int_time'], force_linear=config['plotting']['force_linear'], save_dir=output_path,
+                    result_dict['int_time'], force_linear=config['plotting']['force_linear'],
+                    plot_stars=config["plotting"]["plot_stars"], fill=config["plotting"]["fill"], save_dir=output_path,
                     save_name=save_name)
