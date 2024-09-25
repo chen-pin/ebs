e56ccdc33d8c8998ef744a3b1b77373b458df07b
Pin Chen <Pin.Chen@jpl.nasa.gov>
2024-09-24 18:05:18 -0700

    Confirm MCMC mode runs
    Used <python run_mcmc_ebs.py inputs/config_mcmc.yml> command to launch
    MCMC test run.  Had to first create a <temporary> subdirectory.

--- ebs/error_budget.py
+++ ebs/error_budget.py
@@ -276,7 +276,7 @@ class ErrorBudget(ExosimsWrapper):
         self.contrast = read_csv(filename=contrast_path, skiprows=1)[:, 1]
         self.throughput = read_csv(filename=throughput_path, skiprows=1)[:, 1]
         self.wfe = read_csv(filename=os.path.join(self.input_dir, config['input_files']['wfe']), skiprows=1)
-        self.wfsc_factor = read_csv(filename=os.path.join(self.input_dir, config['input_files']['wfsc']), skiprows=1)
+        self.wfsc_factor = read_csv(filename=os.path.join(self.input_dir, config['input_files']['wfsc_factor']), skiprows=1)
         self.sensitivity = read_csv(filename=os.path.join(self.input_dir, config['input_files']['sensitivity']),
                                     skiprows=1)
 
