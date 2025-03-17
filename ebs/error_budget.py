aa0b73aecf82e78c6e27c0c67e1bbe977da06fc3
Sarah Steiger <ssteiger@stsci.edu>
2025-03-17 10:24:43 -0400

    remove unused imports

--- ebs/error_budget.py
+++ ebs/error_budget.py
@@ -1,7 +1,7 @@
 """Exposure-time calculations based on coronagraphic input parameters
 
 """
-import os, time, shutil, datetime
+import os, time, shutil
 import numpy as np
 import yaml 
 from multiprocessing import Pool
@@ -12,8 +12,6 @@ import EXOSIMS.MissionSim as ems
 from copy import deepcopy
 from ebs.utils import read_csv
 import ebs.log_pdf as pdf
-import matplotlib.pyplot as plt
-import pickle
 
 
 class ExosimsWrapper:
