8e441ebc45e9bdabc581c4c70d3f3d8c9e95a817
Sarah Steiger <ssteiger@stsci.edu>
2025-03-17 13:00:29 -0400

    update util docstrings.

--- ebs/utils.py
+++ ebs/utils.py
@@ -2,16 +2,26 @@ import numpy as np
 
 
 def read_csv(filename, skiprows=0):
-    """
-    returns the data contained in a csv file as a numpy array.
+    """Returns the data contained in a CSV file as a numpy array.
+
     Values are separated by commas and rows are separated by newlines
-    :param filename: str, fully qualified path to the csv file
-    :param skiprows: int, number of rows to skip at the top of the csv file
-    :return: numpy array, data contained in the csv file
+
+    Parameters
+    ----------
+    filename: str
+        Fully qualified path to the csv file.
+    skiprows: int
+         number of rows to skip at the top of the CSV file.
+
+    Returns
+    -------
+    data: np.ndarray
+        Data contained in the CSV file.
     """
     data = np.loadtxt(open(filename, "rb"), delimiter=',', skiprows=skiprows)
     return data
 
 
 def write_csv(savename, data, header):
+    """Write CSV header and data to savename"""
     np.savetxt(savename, data, delimiter=',', header=header)
